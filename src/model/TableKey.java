c97b2c27651413d8d2adf2a4fc8e977a022cc0fc
kaiti <122762348+kait-bit@users.noreply.github.com>
2025-02-23 23:07:14 -0600

    created a custom key for use by SkillTable

--- src/model/TableKey.java
+++ src/model/TableKey.java
@@ -1,4 +1,10 @@
 package model;
 
-public class TableKey {
-}
+/**
+ * The TableKey record represents a custom key for use by the {@link SkillTable} class.
+ * The key enables skill -> method -> xp searching.
+ *
+ * @param skill - skill name
+ * @param method - grinding method
+ */
+public record TableKey(String skill, String method) { }
