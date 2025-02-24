8f3a675bdbe5a63245b738df5b6a26ed05c903f9
kaiti <122762348+kait-bit@users.noreply.github.com>
2025-02-24 01:13:30 -0600

    added test for XpCalc class

--- src/tester/XpCalcTests.java
+++ src/tester/XpCalcTests.java
@@ -1,4 +1,15 @@
 package tester;
 
+import model.XpCalc;
+import model.SkillMap;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class XpCalcTests {
+
+    @Test public void testXpCalc() {
+        SkillMap skillMap = new SkillMap();
+        XpCalc xpCalc = new XpCalc(skillMap, 2000.0, 50.0, 2.0);
+        Assertions.assertEquals(4.0, xpCalc.calcDays("Agility", "Varrock Rooftop Course"));
+    }
 }
