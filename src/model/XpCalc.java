65ae976fc8b13bc3f69d6d0320d06700ccf7e8c5
kaiti <122762348+kait-bit@users.noreply.github.com>
2025-02-25 22:41:59 -0600

    tweaked formula

--- src/model/XpCalc.java
+++ src/model/XpCalc.java
@@ -1,5 +1,7 @@
 package model;
 
+import java.text.DecimalFormat;
+
 /**
  * The XpCalc class performs xp calculations based on the {@link SkillMap} and user input.
  */
@@ -9,22 +11,24 @@ public class XpCalc {
     private double currXp;
     private double goalLevel;
     private double hoursPerDay;
+    private DecimalFormat df;
 
     public XpCalc(SkillMap map, double currXp, double goalLevel, double hoursPerDay) {
         this.map = map;
         this.currXp = currXp;
         this.goalLevel = goalLevel;
         this.hoursPerDay = hoursPerDay;
+        this.df = new DecimalFormat("0.0");
     }
 
     // Finds how many days it will take to reach the desired level
-    public double calcDays(String skill, String method) {
+    public String calcDays(String skill, String method) {
         /*
         Calculates total xp needed for goal level
         OSRS experience formula, ignoring floor functions
         Accurate +/- 14 xp
          */
-        double goalXp = (0.125 * (Math.pow(goalLevel, 2) - goalLevel + 600 * ((Math.pow(2, goalLevel / 7.0) - Math.pow(2, 1.0 / 7.0))) / (Math.pow(2, 1.0 / 7.0) - 1.0))) - currXp;
+        double goalXp = (0.125 * (Math.pow(goalLevel, 2) - goalLevel + 600 * ((Math.pow(2, goalLevel / 7.0) - Math.pow(2, 1.0 / 7.0))) / (Math.pow(2, 1.0 / 7.0) - 1.0)));
 
         // Find xp still needed based on current xp
         double neededXp = (goalXp - currXp);
@@ -32,7 +36,9 @@ public class XpCalc {
         // Get xpPerHour from skill map based on chosen skill and grinding method
         int xpPerHour = map.getXp(skill, method);
 
+        double days = (neededXp / xpPerHour) / hoursPerDay;
+
         // Return number of days it will take to reach goal level based on grinding method and hours per day
-        return Math.round((neededXp / xpPerHour) / hoursPerDay);
+        return df.format(days);
     }
 }
