b27e1d709d7a86092b6af03a28930de7065a5655
kaiti <122762348+kait-bit@users.noreply.github.com>
2025-02-25 22:41:10 -0600

    packed components and set default settings for the frame

--- src/view/SkillAppView.java
+++ src/view/SkillAppView.java
@@ -1,4 +1,37 @@
 package view;
 
-public class SkillAppView {
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ * The SkillAppView class represents the frame for the GUI of the OSRS Skill Calculator application.
+ * <p><br>Extends {@link JFrame}
+ */
+public class SkillAppView extends JFrame {
+
+    public final SkillAppForm form;
+
+    public SkillAppView() {
+        this.form = new SkillAppForm();
+
+        // Get the underlying panel and set content
+        JPanel content = form.getPanel();
+        this.setContentPane(content);
+
+        // Set default dimensions
+        this.setPreferredSize(new Dimension(400, 400));
+
+        // Pack components
+        this.pack();
+
+        // Set title
+        this.setTitle("OSRS Skill Calculator");
+
+        // Destroy the window when X is clicked
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+    public SkillAppForm getForm() {
+        return form;
+    }
 }
