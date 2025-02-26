335107906e00970eb26266739811c06318c845d8
kaiti <122762348+kait-bit@users.noreply.github.com>
2025-02-25 22:48:36 -0600

    added main method

--- src/App.java
+++ src/App.java
@@ -1,2 +1,16 @@
+import controller.SkillAppController;
+import model.SkillMap;
+import view.SkillAppView;
+
+/**
+ * The App class creates instances of {@link SkillMap}, {@link SkillAppView}, and {@link SkillAppController}.
+ */
+
 public class App {
+    public static void main(String[] args) {
+        SkillMap skillMap = new SkillMap();
+        SkillAppView skillAppView = new SkillAppView();
+        SkillAppController skillAppController = new SkillAppController(skillMap, skillAppView);
+        skillAppView.setVisible(true);
+    }
 }
